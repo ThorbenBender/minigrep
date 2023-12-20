253ff453ad003353cfb206a1cf296a6d7fe82944
Thorben Bender <bender.thorben0@gmail.com>
2023-12-20 11:09:05 +0100

    Use iterator instead of a vector

--- src/lib.rs
+++ src/lib.rs
@@ -14,25 +14,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut lines: Vec<&str> = vec![];
     let query = query.to_lowercase();
-    for line in contents.lines() {
-        let lower_case_line = line.to_lowercase();
-        if lower_case_line.contains(&query) {
-            lines.push(line);
-        }
-    }
-    lines
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut lines: Vec<&str> = vec![];
-    for line in contents.lines() {
-        if line.contains(query) {
-            lines.push(line);
-        }
-    }
-    lines
+    contents.lines().filter(|x| x.contains(query)).collect()
 }
 
 pub struct Config {
@@ -42,12 +32,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enought arguments!!");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path string"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
